defcf3cbc25db38fd5831031415f71df196d5129
believer1 <56217003+believer1@users.noreply.github.com>
2019-10-10 20:49:34 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -25,7 +25,7 @@ im a group manager bot maintained by  [this person](tg://user?id={}).
 Want to add me to your group? [Click here](https://telegram.me/Terminator_ProBot?startgroup=add) 
 Enjoy the World 😎
 
-want to join support group [Click here](https://https://t.me/Terminator_Support) 
+want to join support group [Click here](https://t.me/Terminator_Support). 
 For more commands click /help...
 
 
