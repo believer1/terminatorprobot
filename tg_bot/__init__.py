21e16b1a24ebdf29d93a92fcf249255c8f5e7f80
believer1 <56217003+believer1@users.noreply.github.com>
2019-11-17 20:15:10 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -57,7 +57,7 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    API_WEATHER = os.environ.get('API_OPENWEATHER', None)
+    API_WEATHER = os.environ.get('API_OPENWEATHER', False)
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
 
 else:
